a041a7bc47b90b20e27613c84aa939d860459e91
Martinos <chabotm@gmail.com>
2011-01-29 21:15:25 -0500

    Cosmetic

--- lib/remote_cp.rb
+++ lib/remote_cp.rb
@@ -6,27 +6,22 @@ module RemoteCp
     else
       full_path = File.expand_path(filename)
       basename = File.basename(full_path)
-
       content = File.open(full_path)
     end
     s3_connect
-
     S3Object.store("file_name.txt", basename, 'RemoteClipboard')
     S3Object.store("content", content, 'RemoteClipboard')
   end
 
   def rmp
     s3_connect
-
     file_name = S3Object.find("file_name.txt", 'RemoteClipboard').value
-
     if File.exist?(file_name)
       puts "#{file_name} already exist."
       print "Do you want to replace it (y/n)? "
       res = $stdin.gets.chomp
       return unless res == "y"
     end
-
     content = S3Object.find('content', 'RemoteClipboard').value
     File.open(file_name, "w+") {|f| f << content}
     puts "#{file_name} copied."
